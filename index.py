21d70c571898a35d95539bf740c976ab4b91ff53
Marc Bellario <bellario.marc@yahoo.com>
2015-11-16 13:07:54 -0600

    simplify template
    added itsdangerous

--- index.py
+++ index.py
@@ -21,8 +21,8 @@ def index():
 #    userinputs = [x for x in handle.mycollection.find()]
 #    return render_template('index.html', userinputs=userinputs)
     print "hello-flask-again"
-    return "hello-flask-again"
-#    return render_template('index.html')
+#    return "hello-flask-again"
+    return render_template('index1.html')
 
 @app.route("/write", methods=['POST'])
 def write():
