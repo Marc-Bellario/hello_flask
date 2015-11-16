efd1f8e206593db165d601b132c5f0e39fd2ed7c
Marc Bellario <bellario.marc@yahoo.com>
2015-11-16 17:48:05 -0600

    and again
    dealing with connection string some more

--- index.py
+++ index.py
@@ -6,6 +6,7 @@ def connect():
 # Substitute the 5 pieces of information you got when creating
 # the Mongo DB Database (underlined in red in the screenshots)
 # Obviously, do not store your password as plaintext in practice
+'''
     print "start connection"
     try:
         connection = MongoClient("mongodb://ds043262.mongolab.com:43262")
@@ -17,18 +18,18 @@ def connect():
         print " - init exception value: ", e
         handle = None
     return handle
+'''
 
-
-handle = connection()
-#handle = MongoClient('mongodb://admin010101:010101admin@ds043262.mongolab.com:43262/code101')
-
+#connection = connection()
+connection = MongoClient('mongodb://admin010101:010101admin@ds043262.mongolab.com:43262/code101')
+db = connection.code101.ctec121
 # Bind our index page to both www.domain.com/ and www.domain.com/index
 @app.route("/index" ,methods=['GET'])
 @app.route("/", methods=['GET'])
 def index():
     print "hello-flask-again"
     try:
-        userinputs = [x for x in handle.mycollection.find()]
+        userinputs = [x for x in db.find()]
     except Exception as e:
         print app
         print e.args      # arguments stored in .args
@@ -40,12 +41,12 @@ def index():
 @app.route("/write", methods=['POST'])
 def write():
     userinput = request.form.get("userinput")
-    oid = handle.mycollection.insert({"message":userinput})
+    oid = db.insert({"message":userinput})
     return redirect ("/")
 
 @app.route("/deleteall", methods=['GET'])
 def deleteall():
-    handle.mycollection.remove()
+    db.remove()
     return redirect ("/")
 
 # Remove the "debug=True" for production
